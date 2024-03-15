2f0ff4e76bd338114adcb8b1a40cc866e608fd0d
Felix Jensen <felix.jensen@mindmatters.de>
2024-03-15 10:09:47 +0100

    Try sending a map via trpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,12 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { z } from 'zod';
-import { db } from './db.js';
-import { publicProcedure, router } from './trpc.js';
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { z } from "zod";
+import { db } from "./db.js";
+import { publicProcedure, router } from "./trpc.js";
+
+const userSchema = z.object({
+  name: z.string(),
+  hobbies: z.map(z.string(), z.number().min(1).max(5)),
+});
 
 const appRouter = router({
   user: {
@@ -14,13 +19,11 @@ const appRouter = router({
       const user = await db.user.findById(input);
       return user;
     }),
-    create: publicProcedure
-      .input(z.object({ name: z.string() }))
-      .mutation(async (opts) => {
-        const { input } = opts;
-        const user = await db.user.create(input);
-        return user;
-      }),
+    create: publicProcedure.input(userSchema).mutation(async (opts) => {
+      const { input } = opts;
+      const user = await db.user.create(input);
+      return user;
+    }),
   },
 });
 
@@ -32,4 +35,3 @@ const server = createHTTPServer({
 });
 
 server.listen(3434);
-
